8da8d1a31bb9bcfc26d9f4fac11bb776ea8532f0
Rachel Lew <rlew07@gmail.com>
2020-05-21 17:40:34 -0600

    Update sidebar

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -9,8 +9,7 @@ export default class Sidebar extends Component {
           <aside id="colorlib-aside" className="border js-fullheight">
             <div className="text-center">
               <div className="author-img" style={{backgroundImage: 'url(images/about.jpg)'}} />
-              <h1 id="colorlib-logo"><a href="index.html">Dhruv Barochiya</a></h1>
-              <span className="email"><i className="icon-mail"></i> Dhruv.barochiya34788@gmail.com</span>
+              <h1 id="colorlib-logo"><a href="index.html">Rachel Lew</a></h1>
             </div>
             <nav id="colorlib-main-menu" role="navigation" className="navbar">
               <div id="navbar" className="collapse">
@@ -25,22 +24,75 @@ export default class Sidebar extends Component {
             </nav>
             <nav id="colorlib-main-menu">
               <ul>
-                <li><a href="https://www.facebook.com/dhruv.barochia" target="_blank" rel="noopener noreferrer"><i className="icon-facebook2" /></a></li>
-                <li><a href="https://twitter.com/ddbarochiya" target="_blank" rel="noopener noreferrer"><i className="icon-twitter2" /></a></li>
-                <li><a href="https://www.instagram.com/ddbarochiya/" target="_blank" rel="noopener noreferrer"><i className="icon-instagram" /></a></li>
-                <li><a href="https://www.linkedin.com/in/dhruv-b-545b52a9/" target="_blank" rel="noopener noreferrer"><i className="icon-linkedin2" /></a></li>
-                <li><a href="https://github.com/Dhruv34788" target="_blank" rel="noopener noreferrer"><i className="icon-github"></i></a></li>
-                <li><a href="https://medium.com/@dhruv.barochia34788" target="_blank" rel="noopener noreferrer"><i className="icon-blogger2"></i></a></li>
+                <li><a href="https://www.linkedin.com/in/rachel-ann-lew/" target="_blank" rel="noopener noreferrer"><i className="icon-linkedin2" /></a></li>
+                <li><a href="https://github.com/rlew421" target="_blank" rel="noopener noreferrer"><i className="icon-github"></i></a></li>
+                <li><a href="mailto: rlew07@gmail.com" target="_blank" rel="noopener noreferrer"><i className="icon-mail"></i></a></li>
               </ul>
             </nav>
             <div className="colorlib-footer">
-              <p><small>
-                  Made with <i className="icon-heart" aria-hidden="true" /> and <i className="icon-beer" aria-hidden="true"></i><br></br>
-                  Thanks <a href="https://colorlib.com" target="_blank" rel="noopener noreferrer">Colorlib</a> for inspiration 
-              </small></p>
-              <p><small>
-                Something coming soon !!
-              </small></p>
+              <p>
+                <small>
+                  <div>
+                    Template from{" "}
+                    <a
+                      href="https://www.freecodecamp.org/news/portfolio-app-using-react-618814e35843/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Dhruv Barochiya
+                    </a>
+                  </div>
+                  <div>
+                    Inspired by{" "}
+                    <a
+                      href="https://colorlib.com"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Colorlib
+                    </a>{" "}
+                  </div>
+                  <div>
+                    Icons made by{" "}
+                    <a
+                      href="https://www.flaticon.com/authors/freepik"
+                      title="Freepik"
+                    >
+                      Freepik
+                    </a>{" "}
+                    from{" "}
+                    <a href="https://www.flaticon.com/" title="Flaticon">
+                      www.flaticon.com
+                    </a>
+                  </div>
+                  <div>
+                    Icons made by{" "}
+                    <a
+                      href="https://www.flaticon.com/authors/photo3idea-studio"
+                      title="photo3idea_studio"
+                    >
+                      photo3idea_studio
+                    </a>{" "}
+                    from{" "}
+                    <a href="https://www.flaticon.com/" title="Flaticon">
+                      www.flaticon.com
+                    </a>
+                  </div>
+                  <div>
+                    Icons made by{" "}
+                    <a
+                      href="https://www.flaticon.com/authors/smashicons"
+                      title="Smashicons"
+                    >
+                      Smashicons
+                    </a>{" "}
+                    from{" "}
+                    <a href="https://www.flaticon.com/" title="Flaticon">
+                      www.flaticon.com
+                    </a>
+                  </div>
+                </small>
+              </p>
             </div>
           </aside>
         </div>
