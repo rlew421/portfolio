929d7f17f1d2c4b3f2c97b5e3d1c7f7140394755
Rachel Lew <rlew07@gmail.com>
2020-05-22 20:43:18 -0600

    Updates

---
+++ precache-manifest.efa4e3c37f2d54855859fb007f5eccf6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce782fe29f65ce81eb6a",
+    "url": "/portfolio/static/css/main.8fc4a845.chunk.css"
+  },
+  {
+    "revision": "ce782fe29f65ce81eb6a",
+    "url": "/portfolio/static/js/main.ce782fe2.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "02342793ee9d91a2a6d1",
+    "url": "/portfolio/static/js/2.02342793.chunk.js"
+  },
+  {
+    "revision": "e0a759d2831cec4225ac75a1c737ed84",
+    "url": "/portfolio/index.html"
+  }
+];
